034fd58c0c56c328ec1daaa217644b2977d14126
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-06-09 05:27:06 +0000

    Initial import.

---
+++ trunk/AudioPulseCalibration/src/AudioPulseCalibration/AudioPulse/org/AudioPulseCalibrationActivity.java
@@ -0,0 +1,13 @@
+package AudioPulseCalibration.AudioPulse.org;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AudioPulseCalibrationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
