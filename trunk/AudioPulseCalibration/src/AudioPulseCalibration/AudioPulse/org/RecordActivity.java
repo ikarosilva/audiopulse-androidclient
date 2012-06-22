f55a663c009b199a401efbe857c2fa5d15dc6a38
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-06-22 03:33:31 +0000

    Fixed issues with plotting activity and did some cleaning up.

--- trunk/AudioPulseCalibration/src/AudioPulseCalibration/AudioPulse/org/RecordActivity.java
+++ trunk/AudioPulseCalibration/src/AudioPulseCalibration/AudioPulse/org/RecordActivity.java
@@ -1,7 +1,45 @@
+/* ===========================================================
+ * SanaAudioPulse : a free platform for teleaudiology.
+ *              
+ * ===========================================================
+ *
+ * (C) Copyright 2012, by Sana AudioPulse
+ *
+ * Project Info:
+ *    SanaAudioPulse: http://code.google.com/p/audiopulse/
+ *    Sana: http://sana.mit.edu/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * [Android is a trademark of Google Inc.]
+ *
+ * -----------------
+ * RecordActivity.java
+ * -----------------
+ * (C) Copyright 2012, by SanaAudioPulse
+ *
+ * Original Author:  Ikaro Silva
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * Check: http://code.google.com/p/audiopulse/source/list
+ */ 
+
 package AudioPulseCalibration.AudioPulse.org;
 
-import AudioPulseCalibration.AudioPulse.org.DeviationRendererDemo02Activity;
-
+import AudioPulseCalibration.AudioPulse.org.PlotWaveformActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.AudioFormat;
@@ -127,7 +165,7 @@ public class RecordActivity extends Activity {
 	    Log.v(TAG, "Sample " + i + " = " + audioBuffer[i]);
 		}
 		Log.v(TAG, "plotting data...");
-		Intent intent = new Intent(this.getApplicationContext(), DeviationRendererDemo02Activity.class);
+		Intent intent = new Intent(this.getApplicationContext(), PlotWaveformActivity.class);
         startActivity(intent);
 	}
 }
