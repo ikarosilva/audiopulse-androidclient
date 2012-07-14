7732062dccf7c711397600dddc2caf3d20022b8a
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-07-14 03:56:27 +0000

    re-arranged packages

---
+++ AudioPulseCalibration/src/org/audiopulse/io/ReportStatusHandler.java
@@ -0,0 +1,29 @@
+package org.audiopulse.io;
+
+import android.os.Handler;
+import android.os.Message;
+
+public class ReportStatusHandler extends Handler
+{
+	public static final String TAG = "ReportStatusHandler";
+	private ThreadedPlayRecActivity parentActivity = null; 
+	
+	public ReportStatusHandler(ThreadedPlayRecActivity inParentActivity)
+	{
+		//Registering handler in parent activity 
+		parentActivity = inParentActivity;
+	}
+
+	@Override
+	public void handleMessage(Message msg) 
+	{
+		String pm = Utils.getStringFromABundle(msg.getData());		
+		this.printMessage(pm);
+	}
+
+	private void printMessage(String str)
+	{
+		//Printing status
+		parentActivity.appendText(str);
+	}
+}
