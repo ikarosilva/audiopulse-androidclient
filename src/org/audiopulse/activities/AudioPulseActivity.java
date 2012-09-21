909727e612de90a5d47cf652c4e53f64249f27ed
ozydingo@gmail.com <ozydingo@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-09-21 18:39:18 +0000

    Modified AudioPulseActivity.java to store existing airplane mode state only on the first instance, and restore it when the task root activity is destroyed

--- src/org/audiopulse/activities/AudioPulseActivity.java
+++ src/org/audiopulse/activities/AudioPulseActivity.java
@@ -2,30 +2,69 @@ package org.audiopulse.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.widget.Toast;
 
 public class AudioPulseActivity extends Activity{
 	
 	public static final String TAG = "AudioPulse Activity";
-	
+	public static boolean userAirplaneMode;
+	public static boolean initialized = false;
 		
-	public void setAirplaneMode(Boolean enable){ 
-    	try{ 
-	    	boolean isEnabled = Settings.System.getInt(
-	    			getBaseContext().getContentResolver(),
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		if (!initialized) {
+			//code block for things that will only happen on the first launch
+			userAirplaneMode = Settings.System.getInt( getContentResolver(),
 	    			Settings.System.AIRPLANE_MODE_ON, 0
-	    			) == 1; 
-	    	// If airplane mode is on, value 0, else value is 1 
+	    			) == 1;
+			
+			initialized = true;
+		}
+		 
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+		setAirplaneMode(true);
+		//TODO: wait for airplane mode (& volume, other settings) to be set before continuing
+	}
+	
+	
+	
+	@Override
+	protected void onDestroy() {
+		if (isTaskRoot() && isFinishing()) {
+			setAirplaneMode(userAirplaneMode);
+		}
+		super.onDestroy();
+	}
+		
+	public boolean setAirplaneMode(Boolean enable){ 
+    	try{ 
+    		if (enable == (Settings.System.getInt(getContentResolver(), 
+    		    	Settings.System.AIRPLANE_MODE_ON)==1)) {
+    			return true; //don't write setting if it's already what we want
+    		}
+
 	    	Settings.System.putInt( 
-		    	getBaseContext().getContentResolver(), 
+		    	getContentResolver(), 
 		    	Settings.System.AIRPLANE_MODE_ON, enable ? 1 : 0); 
-	    	// Reload when the mode is changed each time by sending Intent 
+
+	    	//broadcast event.
+	    	//TODO: establish listener so we know if someone else turned it off
 	    	Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED); 
-		    	intent.putExtra("state", !isEnabled); 
+		    	intent.putExtra("state", !enable); 
 	    	getBaseContext().sendBroadcast(intent); 
+	    	return true;
     	} catch(Exception e) { 
-	    	Toast.makeText(getBaseContext(), "exception:"+e.toString(), Toast.LENGTH_LONG).show(); 
+	    	Toast.makeText(this, "exception:"+e.toString(), Toast.LENGTH_LONG).show();
+	    	return false;
     	} 
 	} 
 }
