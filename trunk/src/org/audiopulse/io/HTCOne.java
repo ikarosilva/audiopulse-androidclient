960fdcb17338c3474cfc8e9f03dc6db5b1cecaf2
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-10-21 15:43:52 +0000

    Java class for HTC One phone

---
+++ trunk/src/org/audiopulse/io/HTCOne.java
@@ -0,0 +1,60 @@
+package org.audiopulse.io;
+import org.audiopulse.io.AcousticDevice.ioDevice;
+
+public class HTCOne extends MobilePhone {
+
+private double maxAmplitude; //Max amplitude of a 1 kHz that does not clip speaker
+private int minAttenuation;
+private final int calFreq=1000;
+private final String name="HTCOne";
+private AcousticDevice.ioDevice acousticDevice;
+	
+public enum deviceAttn{
+	//Only include devices that were measured for attn 
+	DUMMY(20),
+	ER10C(40);
+	
+	public int attn;
+	deviceAttn(int atten){
+		this.attn=atten;
+	}	
+}
+
+	public HTCOne(HTCOne.deviceAttn deviceAttn, AcousticDevice.ioDevice acousticDevice){
+	
+		this.minAttenuation=deviceAttn.attn;
+		this.maxAmplitude=getMaxAmp(minAttenuation);
+		this.acousticDevice= acousticDevice;
+	}
+
+	@Override
+	public double getMaxAmplitude() {
+		// TODO Auto-generated method stub
+		return this.maxAmplitude;
+	}
+
+	@Override
+	public int getMinAttenuation() {
+		// TODO Auto-generated method stub
+		return this.minAttenuation;
+	}
+
+	@Override
+	public int getCalFreq() {
+		// TODO Auto-generated method stub
+		return this.calFreq;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	@Override
+	public ioDevice getAcousticDevice() {
+		// TODO Auto-generated method stub
+		return this.acousticDevice;
+	}
+
+}
