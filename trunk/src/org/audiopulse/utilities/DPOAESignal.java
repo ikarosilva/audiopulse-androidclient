dce091a577f00e047a0291065f050bff54686e0a
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-10-21 15:43:08 +0000

    DPOAE stimulus that uses phone  configuration to set stimulus.

--- trunk/src/org/audiopulse/utilities/DPOAESignal.java
+++ trunk/src/org/audiopulse/utilities/DPOAESignal.java
@@ -39,34 +39,36 @@
 
 package org.audiopulse.utilities;
 
-public class DPOAESignal {
+import org.audiopulse.io.MobilePhone;
 
-	private double[] f;
-	private double[] A; //returned Amplitudes are in intensity
-	private double f12; //Expected response frequency
-	private String protocol;
+public class DPOAESignal extends PeriodicSeries{
+
+	public double[] f;
+	public double[] A; 
+	public double f12; //Expected response frequency
+	public String protocol;
 	
 	public static enum protocolBioLogic {
 		//Generate a specific set of DPOAE stimuli based on the same parameters from 
 		//that of the Bio-Logic Otoacoustic emissions Report (2012)
+		
+		//Amplitudes are in dB SPL!!
 		F8k(6516,7969,64.8,54.9),
 		F6k(4594,5625,64.8,56.6),
 		F4k(3281,3984,64.8,55.6),
 		F3k(2297,2813,64.6,55.1),
 		F2k(1641,2016,64.4,53.4);
 
-		private double f1;
-		private double f2;
-		private double A1;	//Amplitudes are in dB
-		private double A2;
+		private double[] f=new double[2];
+		private double[] A=new double[2];	//Amplitudes are in dB
 		private double f12; //Expected response frequency
 		private String protocol="BioLogic";
 		protocolBioLogic(double f1,double f2, double A1, double A2) {
-			this.f1=f1;
-			this.f2=f2;
-			this.A1=Math.pow(10,A1/20); //Convert amplitudes to normalized intensity
-			this.A2=Math.pow(10,A2/20);
-			this.f12=2*f1-f2;
+			f[0]=f1;
+			f[1]=f2;
+			A[0]=A1; 
+			A[1]=A2;
+			f12=2*f[0]-f[1];
 		}
 	}
 
@@ -79,48 +81,32 @@ public class DPOAESignal {
 		F4k(4000),
 		F3k(3000),
 		F2k(2000);
-		private double f1;
-		private double f2;
-		private double A1; //Amplitudes are in dB
-		private double A2;
+		private double[] f=new double[2];
+		private double[] A=new double[2]; //Amplitudes are in dB
 		private double f12; //Expected response frequency
 		private String protocol="HOAE";
 		protocolHOAE(double f2) {
-			this.f1=f2/1.2;
-			this.f2=f2;
-			this.A1=Math.pow(10,65/20); //Convert amplitudes to normalized intensity
-			this.A2=Math.pow(10,65/20);
-			this.f12=2*f1-f2;
+			f[0]=f2/1.2;
+			f[1]=f2;
+			A[0]=65; 
+			A[1]=65;
+			f12=2*f[0]-f[1];
 		}
 	}
 
-	public DPOAESignal(protocolBioLogic dpoae){
-		double[] f={dpoae.f1,dpoae.f2};
-		this.f=f;
-		double[] A= {dpoae.A1, dpoae.A2};
-		this.A=A;
-		this.f12=dpoae.f12;
-		this.protocol=dpoae.protocol;
+	public DPOAESignal(protocolBioLogic dpoae,int N, double Fs, MobilePhone phone, int chConf){
+	
+		super(N,Fs,dpoae.f,dpoae.A,chConf);
+		f12=dpoae.f12;
+		protocol=dpoae.protocol;
 	}
-	public DPOAESignal(protocolHOAE dpoae){
-		double[] f={dpoae.f1,dpoae.f2};
-		this.f=f;
-		double[] A= {dpoae.A1, dpoae.A2};
-		this.A=A;
-		this.f12=dpoae.f12;
-		this.protocol=dpoae.protocol;
+	public DPOAESignal(protocolHOAE dpoae,int N, double Fs, MobilePhone phone, int chConf){
+		super(N,Fs,dpoae.f,dpoae.A,chConf);
+		f12=dpoae.f12;
+		protocol=dpoae.protocol;
 	}
 	
-	public double[] getStimulusFrequency(){
-		return this.f;
-	}
-	public double[] getStimulusAmplitude(){
-		return this.A;
-	}
-	public double getExpectedResponseFrequency(){
-		return this.f12;
-	}
-	public String getProtocol(){
-		return this.protocol;
-	}
+	
+	
+	
 }
