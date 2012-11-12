d431009e75f9a0e510f2673a807ce766f118fb1a
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2012-11-12 04:52:33 +0000

    separated dpgram and individual tests into own activities.

--- trunk/src/org/audiopulse/activities/AudioPulseRootActivity.java
+++ trunk/src/org/audiopulse/activities/AudioPulseRootActivity.java
@@ -1,13 +1,50 @@
 package org.audiopulse.activities;
 
+import org.audiopulse.R;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AudioPulseRootActivity extends AudioPulseActivity{
-	
+
+	private Intent audioResultsBundle;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 				 
 	}
 	
+	public void appendText(String str){
+		TextView tv = getTextView(); 
+		tv.setText(tv.getText() + "\n" + str);
+	}
+	
+	public void emptyText(){
+		TextView tv = getTextView();
+		tv.setText("");
+	}
+	
+	private TextView getTextView(){
+		return (TextView)this.findViewById(R.id.text1);
+	}
+	
+	public void plotSpectrum(Bundle audioResultsBundle) {
+		Intent intent = new Intent(this.getApplicationContext(), PlotSpectralActivity.class);
+		intent.putExtras(audioResultsBundle);
+		startActivity(intent);
+	}
+
+	public void plotWaveform() {
+		Intent intent = new Intent(this.getApplicationContext(), PlotWaveformActivity.class);
+		intent.putExtras(audioResultsBundle);
+		startActivity(intent);
+	}
+	
+	public void plotAudiogram() {
+		Intent intent = new Intent(this.getApplicationContext(), PlotAudiogramActivity.class);
+		startActivity(intent);
+	}
+
 }
