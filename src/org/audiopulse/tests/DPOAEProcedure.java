75b463b32e3b24e904f4c97bdf52effea41bc137
ozydingo@gmail.com <ozydingo@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-02-26 14:17:59 +0000

    Added functional example to DPOAEProcedure. Added unimplemented general-purpose calibration functions to TestProcedure.

--- src/org/audiopulse/tests/DPOAEProcedure.java
+++ src/org/audiopulse/tests/DPOAEProcedure.java
@@ -1,5 +1,7 @@
 package org.audiopulse.tests;
 
+import java.util.LinkedList;
+
 import org.audiopulse.activities.TestActivity;
 import org.audiopulse.hardware.AcousticConverter;
 import org.audiopulse.hardware.MobilePhone;
@@ -8,38 +10,97 @@ import org.audiopulse.utilities.Signals;
 
 //TODO: put full DPOAE functionality here, fix things as needed
 public class DPOAEProcedure extends TestProcedure {
-
+	final String TAG = "DPOAEProcedure";
+	LinkedList<DPOAEParameters> testList = new LinkedList<DPOAEProcedure.DPOAEParameters>();
+	
 	public DPOAEProcedure(TestActivity parent) {
 		super(parent);
-		// TODO Auto-generated constructor stub
+		testList.add(BiologicParameters.F2k);
+		testList.add(BiologicParameters.F3k);
+		testList.add(BiologicParameters.F4k);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		clearLog();
+		int numTests = testList.size();
+		CalibrationParameters[] cal = new CalibrationParameters[numTests];
+		for (int test=0; test<numTests; test++) {
+			DPOAEParameters params = testList.get(test);
+			cal[test] = calibrate(params);
+		}
+		
+		double[][] results = new double[numTests][];
+		for (int test=0; test<numTests; test++) {
+			DPOAEParameters params = testList.poll();
+			logToUI("Running " + params.toString());
+			double[][] probe = params.createStimulus(playbackSampleFrequency, hardware);
+			testIO.setPlaybackAndRecording(playbackSampleFrequency, probe, 
+					recordingSampleFrequency);
+			results[test] = testIO.acquire();
+		}
+		//TODO: analyze results, store results & analysis
+	}
+	
+	//calibrate one set of DPOAE tones (f1,f2,dp)
+	private CalibrationParameters calibrate(DPOAEParameters params) {
+		double[] tone;
+		//1. For f1, f2, dp: generate test tone
+		//2. Prepare media playback & recording
+		//3. Play & record
+		//4. Look for onset & offset in recording
+		//5. Determine level between onset & offset
+		//6. Save result.
+		
+		double level = 65;		//linear calibration level (dB SPL)
+		double duration = 0.4;	//calibration tone duration (s)
+
+		//get in-ear gain for f1 tone
+		tone = Signals.tone(playbackSampleFrequency, params.f1, duration);
+		tone = hardware.setOutputLevel(tone, level);
+		double[][] f1Tone = AudioSignal.monoToStereoLeft(tone);
+		double f1Gain = calibrateTone(f1Tone, level);
+		logToUI("f1 ("+ String.format("%.0f Hz",params.f1) +") gain: "
+				+ String.format("%.1f dB",f1Gain));
+		
+		//get in-ear gain for f2 tone
+		tone = Signals.tone(playbackSampleFrequency, params.f2, duration);
+		tone = hardware.setOutputLevel(tone, level);
+		double[][] f2Tone = AudioSignal.monoToStereoRight(tone);
+		double f2Gain = calibrateTone(f2Tone, level);
+		logToUI("f2 ("+ String.format("%.0f Hz",params.f2) +") gain: "
+				+ String.format("%.1f dB",f2Gain));
+		
+		//get in-ear gain for dp tone
+		tone = Signals.tone(playbackSampleFrequency, params.dpFreq, duration);
+		tone = hardware.setOutputLevel(tone, level);
+		double[][] dpTone = AudioSignal.convertToStereo(tone);
+		double dpGain = calibrateTone(dpTone, level);
+		logToUI("DP ("+ String.format("%.0f Hz",params.dpFreq) +") gain: "
+				+ String.format("%.1f dB",dpGain));
+		
+		return new CalibrationParameters(f1Gain, f2Gain, dpGain);
 
 	}
 	
-	//calibrate one set of DPOAE tones
-	private void calibrate(DPOAEParameters params) {
-		double[][] f1Tone = AudioSignal.monoToStereoLeft(
-				params.createF1Tone(playbackSampleFrequency, hardware) );
-		double gainF1 = calibrate(f1Tone);
-		double[][] f2Tone = AudioSignal.monoToStereoRight(
-				params.createF1Tone(playbackSampleFrequency, hardware) );
-		double gainF2 = calibrate(f2Tone);
-	}
-	
-	//calibrate a single tone
-	private double calibrate(double[][] signal) {
+	//get in-ear gain relative to given, expected level
+	private double calibrateTone(double[][] signal, double spl) {
 		testIO.setPlaybackAndRecording(playbackSampleFrequency, signal, 
 				recordingSampleFrequency);
-		//FIXME
-		double aOut = hardware.getOutputLevel(signal[0]) + hardware.getOutputLevel(signal[1]);
 		double[] input = testIO.acquire();
-		double aIn = hardware.getInputLevel(input);
-		return aIn - aOut;
-		
+		double splIn = hardware.getInputLevel(input);
+		return splIn - spl;		
+	}
+	
+	//data for calibration results for a single test frequency
+	public static class CalibrationParameters {
+		public final double f1Gain, f2Gain, dpGain;
+		public CalibrationParameters(double f1Gain, double f2Gain, double dpGain) {
+			this.f1Gain = f1Gain;
+			this.f2Gain = f2Gain;
+			this.dpGain = dpGain;
+		}
+		//TODO: delay / phase if needed (are they?)
 	}
 	
 	//DPOAE parameters from the Bio-Logic OAE Report (2012)
@@ -48,7 +109,7 @@ public class DPOAEProcedure extends TestProcedure {
 		private static final double duration = 0.5;
 		
 		public static final DPOAEParameters F2k =
-			new DPOAEParameters(protocol,2000,duration,6516,7969,64.8,54.9);
+			new DPOAEParameters(protocol,2000,duration,1641,2016,64.4,53.4);
 		public static final DPOAEParameters F3k =
 			new DPOAEParameters(protocol,3000,duration,2297,2813,64.6,55.1);
 		public static final DPOAEParameters F4k =
@@ -120,8 +181,12 @@ public class DPOAEProcedure extends TestProcedure {
 			tone = hardware.setOutputLevel(tone, level2);
 			tone = Signals.fade(sampleFrequency, 0.01, tone);
 			return tone;
-		}
+		}		
 		
+		@Override
+		public String toString() {
+			return protocol + ", " + testFrequency + " Hz";
+		}
 		
 	}
 
