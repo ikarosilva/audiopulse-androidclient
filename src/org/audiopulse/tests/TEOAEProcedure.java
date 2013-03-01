c9f05d03bd0ac80626903faf0835085a9b726a88
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-03-01 05:06:50 +0000

    Modified TEOAE test to fit into the new framework (not fully functional yet).

--- src/org/audiopulse/tests/TEOAEProcedure.java
+++ src/org/audiopulse/tests/TEOAEProcedure.java
@@ -1,58 +1,60 @@
 package org.audiopulse.tests;
 
+import java.io.File;
+
 import org.audiopulse.activities.TestActivity;
 import org.audiopulse.hardware.AcousticConverter;
+import org.audiopulse.io.AudioPulseFileWriter;
+import org.audiopulse.tests.DPOAEProcedure.DPOAEParameters;
 import org.audiopulse.utilities.APAnnotations.UnderConstruction;
 import org.audiopulse.utilities.AudioSignal;
 import org.audiopulse.utilities.Signals;
+
+import android.util.Log;
 
 @UnderConstruction(owner="Ikaro Silva")
 public class TEOAEProcedure extends TestProcedure{
 
 	private final String TAG = "TEOAECalibration";
-	
+
 	private final double stimulusDuration=4;//stimulus duration
-	
+
 	private AcousticConverter converter = new AcousticConverter();
-	
-	
+
+
 	public TEOAEProcedure(TestActivity parentActivity) {
 		super(parentActivity);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	@Override
 	public void run() {
-		//Since (if) we can't synchronize input & output samples, we must calibrate timing
-		//ourselves by looking for the onset and offset of the recorded test signal
+
+		clearLog();
+
+		double[][] results = new double[1][];
+		double sampleFrequency = 16000;
+		logToUI("Running TEOAE");
+		//create {f1, f2} tones in {left, right} channel of stereo stimulus
+		double[][] stimulus = new double[2][];
+		Log.v(TAG,"Generating stimulus");
+		double[] probe = Signals.clickKempMethod(sampleFrequency, 
+				stimulusDuration);
+		Log.v(TAG,"setting probe old leve probe[0]=" + probe[0]);
+		probe = hardware.setOutputLevel(probe, 50);
+		Log.v(TAG," probe new level probe[0]=" + probe[0]);
+		testIO.setPlaybackAndRecording(AudioSignal.convertMonoToShort(probe));
+		double stTime= System.currentTimeMillis();
+		results[1] = testIO.acquire();
+		double ndTime= System.currentTimeMillis();
+		Log.v(TAG,"done acquiring signal in = "  + (ndTime-stTime)/1000 + " secs" );
 		
-		//for now, let's just calibrate amplitude and forget about phase.
-		double[] click;
-		double A0 = 65;		//test amplitude
-		String file;			//rcvd amplitude
-		
-		//1. For f1, f2, dp; generate test tone
-		//2. Prepare media playback & recording
-		//3. Play & record
-		//4. Look for onset & offset in recording
-		//5. Determine level between onset & offset
-		//6. Save result.
-		
-		click = Signals.clickKempMethod(playbackSampleFrequency,stimulusDuration);
-		click = converter.setOutputLevel(click, A0);
-		file = playStimulus(click);
-		logToUI("Generated click stimulus of " + stimulusDuration  + " seconds.");
-		logToUI("File being saved at:" + file);
+		File file= AudioPulseFileWriter.generateFileName("TEOAE","");
+		logToUI("Saving file at:" + file.getAbsolutePath());
+		AudioPulseFileWriter writer= new AudioPulseFileWriter
+				(file,AudioSignal.convertMonoToShort(results[1]));
+		Log.v(TAG,"saving file" );
+		writer.start();
 	}
-	
-	private String playStimulus(double[] click) {
-		testIO.setPlaybackAndRecording(playbackSampleFrequency, AudioSignal.convertToStereo(click), 
-				recordingSampleFrequency, 100, 100);
-		testIO.start();
-		double[] x = testIO.getResult();
-		
-		//TODO: save data x to file and return URI
-		return null;
-	}
-	
+
 }
