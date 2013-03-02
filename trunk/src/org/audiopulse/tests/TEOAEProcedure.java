8aeb2893da4463f4a0dcaf5fc9a2c3745a821c10
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-03-02 00:52:01 +0000

    fixed issues in TEOAEProceure related to to updates in test.IO and bug in setting click duration.

--- trunk/src/org/audiopulse/tests/TEOAEProcedure.java
+++ trunk/src/org/audiopulse/tests/TEOAEProcedure.java
@@ -3,9 +3,7 @@ package org.audiopulse.tests;
 import java.io.File;
 
 import org.audiopulse.activities.TestActivity;
-import org.audiopulse.hardware.AcousticConverter;
 import org.audiopulse.io.AudioPulseFileWriter;
-import org.audiopulse.tests.DPOAEProcedure.DPOAEParameters;
 import org.audiopulse.utilities.APAnnotations.UnderConstruction;
 import org.audiopulse.utilities.AudioSignal;
 import org.audiopulse.utilities.Signals;
@@ -18,10 +16,7 @@ public class TEOAEProcedure extends TestProcedure{
 	private final String TAG = "TEOAECalibration";
 
 	private final double stimulusDuration=4;//stimulus duration
-
-	private AcousticConverter converter = new AcousticConverter();
-
-
+	short[] results;
 	public TEOAEProcedure(TestActivity parentActivity) {
 		super(parentActivity);
 		// TODO Auto-generated constructor stub
@@ -32,11 +27,10 @@ public class TEOAEProcedure extends TestProcedure{
 
 		clearLog();
 
-		double[][] results = new double[1][];
+		
 		double sampleFrequency = 16000;
 		logToUI("Running TEOAE");
 		//create {f1, f2} tones in {left, right} channel of stereo stimulus
-		double[][] stimulus = new double[2][];
 		Log.v(TAG,"Generating stimulus");
 		double[] probe = Signals.clickKempMethod(sampleFrequency, 
 				stimulusDuration);
@@ -45,14 +39,14 @@ public class TEOAEProcedure extends TestProcedure{
 		Log.v(TAG," probe new level probe[0]=" + probe[0]);
 		testIO.setPlaybackAndRecording(AudioSignal.convertMonoToShort(probe));
 		double stTime= System.currentTimeMillis();
-		results[1] = testIO.acquire();
+		results = testIO.acquire();
 		double ndTime= System.currentTimeMillis();
 		Log.v(TAG,"done acquiring signal in = "  + (ndTime-stTime)/1000 + " secs" );
 		
 		File file= AudioPulseFileWriter.generateFileName("TEOAE","");
 		logToUI("Saving file at:" + file.getAbsolutePath());
 		AudioPulseFileWriter writer= new AudioPulseFileWriter
-				(file,AudioSignal.convertMonoToShort(results[1]));
+				(file,results);
 		Log.v(TAG,"saving file" );
 		writer.start();
 	}
