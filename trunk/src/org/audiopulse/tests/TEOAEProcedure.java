a168dbb99fccff98ccd7d9454162d566bf8458cf
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-03-06 01:58:57 +0000

    added framework for analyzing the data and messaging status of thread consistent with DPOAEActivity.

--- trunk/src/org/audiopulse/tests/TEOAEProcedure.java
+++ trunk/src/org/audiopulse/tests/TEOAEProcedure.java
@@ -15,8 +15,7 @@ public class TEOAEProcedure extends TestProcedure{
 
 	private final String TAG = "TEOAECalibration";
 
-	private final double stimulusDuration=4;//stimulus duration
-	short[] results;
+	private final double stimulusDuration=1;//stimulus duration
 	public TEOAEProcedure(TestActivity parentActivity) {
 		super(parentActivity);
 		// TODO Auto-generated constructor stub
@@ -25,8 +24,11 @@ public class TEOAEProcedure extends TestProcedure{
 	@Override
 	public void run() {
 
+		short[] results;
+		double[] DPGRAM;
+		
 		clearLog();
-
+		sendMessage(TestActivity.Messages.PROGRESS);
 		
 		double sampleFrequency = 44100;
 		logToUI("Running TEOAE");
@@ -34,7 +36,7 @@ public class TEOAEProcedure extends TestProcedure{
 		Log.v(TAG,"Generating stimulus");
 		double[] probe = Signals.clickKempMethod(sampleFrequency, 
 				stimulusDuration);
-		Log.v(TAG,"setting probe old leve probe[0]=" + probe[0]);
+		Log.v(TAG,"setting probe old level probe[0]=" + probe[0]);
 		probe = hardware.setOutputLevel(probe, 55);
 		Log.v(TAG," probe new level probe[0]=" + probe[0]);
 		testIO.setPlaybackAndRecording(AudioSignal.convertMonoToShort(probe));
@@ -47,8 +49,24 @@ public class TEOAEProcedure extends TestProcedure{
 		logToUI("Saving file at:" + file.getAbsolutePath());
 		AudioPulseFileWriter writer= new AudioPulseFileWriter
 				(file,results);
-		Log.v(TAG,"saving file" );
+		Log.v(TAG,"saving raw data" );
 		writer.start();
+		
+        sendMessage(TestActivity.Messages.IO_COMPLETE); //Not exactly true becauase we delegate writing of file to another thread...
+		
+        DPGRAM=analyzeResults();
+		sendMessage(TestActivity.Messages.ANALYSIS_COMPLETE);
+		
+		//TODO: Send data back to Activity, the final saving of result will be done when the Activity returns from plotting the processed
+		//data and the user accepts the results. So we need to delete the binary files if the user decides to reject/redo the test
+		sendDPGRAMData(DPGRAM);
+		sendMessage(TestActivity.Messages.PROCEDURE_COMPLETE);
+	}
+	
+	private double[] analyzeResults() {
+		return null;
+		//TODO: define a TEOAEAnalysis call that operates on input arguments
+		//rather than a file location, call it from here
 	}
 
 }
