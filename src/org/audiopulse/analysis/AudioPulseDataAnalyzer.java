3b1dead47ba62aa9c0291b9ecd06778f78c5989d
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-03-07 03:24:23 +0000

    create a separate package for analysis classes

---
+++ src/org/audiopulse/analysis/AudioPulseDataAnalyzer.java
@@ -0,0 +1,22 @@
+package org.audiopulse.analysis;
+
+public interface AudioPulseDataAnalyzer {
+
+	//Some methods have the option to do analysis in either time or spectrum
+	//domain. Return NaN for methods that wont implement
+	//The spetrum is a 2D array where the first dimension is the index of frequency
+	//in Hz
+	
+	//Get the response for a specific frequency
+	double getResponseLevel(short rawdata[],double frequency, double Fs);
+	double getResponseLevel(double dataFFT[][],double frequency);
+	
+	//Get the noise floor for a specific frequency
+	double getNoiseLevel(short rawdata[],double frequency, double Fs);
+	double getNoiseLevel(double dataFFT[][],double frequency);
+	
+	//Get estimated stimulus level for a specific frequency
+	double getStimulusLevel(short rawdata[],double frequency, double Fs);
+	double getStimulusLevel(double dataFFT[][],double frequency);
+	
+}
