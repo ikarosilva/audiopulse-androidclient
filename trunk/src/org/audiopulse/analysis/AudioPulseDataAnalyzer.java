ba8969e034c9bcad520b9759c93102130e4171fa
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-03-07 03:35:46 +0000

    defined public mapping of keys to the HashMap output of AudioPulseDataAnalyzer so that it is publicly accessible to any class fetching the results (such as the plotting activities).

--- trunk/src/org/audiopulse/analysis/AudioPulseDataAnalyzer.java
+++ trunk/src/org/audiopulse/analysis/AudioPulseDataAnalyzer.java
@@ -2,6 +2,22 @@ package org.audiopulse.analysis;
 
 public interface AudioPulseDataAnalyzer {
 
+	
+	    /// -- Define Mapping of Keys to HashMap 
+	    //The purpose of these keys are to define constants that will be accessible by
+		//different packages and clients analyzing the data in the Maps returned 
+		// by the AudioPulseDataAnalyer interface.
+		//Not sure if this is yet the best way, expect changes...	
+		public static final String RESPONSE_2KHZ="resp2kHz";
+		public static final String RESPONSE_3KHZ="resp3kHz";
+		public static final String RESPONSE_4KHZ="resp4kHz";
+		public static final String NOISE_2KHZ="noise2kHz";
+		public static final String NOISE_3KHZ="noise3kHz";
+		public static final String NOISE_4KHZ="noise4kHz";
+		public static final String STIM_2KHZ="stim2kHz";
+		public static final String STIM_3KHZ="stim3kHz";
+		public static final String STIm_4KHZ="stim4kHz";
+		
 	//Some methods have the option to do analysis in either time or spectrum
 	//domain. Return NaN for methods that wont implement
 	//The spetrum is a 2D array where the first dimension is the index of frequency
