00635e69cff912e43140e8804b21ce7f81e8008e
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-03-09 00:31:28 +0000

    changed interface to used HashMap instead of ConcocurrentHashMap (...easier to pass to a bundle because its serializable).

--- trunk/src/org/audiopulse/tests/TEOAEProcedure.java
+++ trunk/src/org/audiopulse/tests/TEOAEProcedure.java
@@ -1,21 +1,30 @@
 package org.audiopulse.tests;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import org.audiopulse.activities.TestActivity;
+import org.audiopulse.analysis.AudioPulseDataAnalyzer;
+import org.audiopulse.analysis.TEOAEKempAnalyzer;
 import org.audiopulse.io.AudioPulseFileWriter;
 import org.audiopulse.utilities.APAnnotations.UnderConstruction;
 import org.audiopulse.utilities.AudioSignal;
 import org.audiopulse.utilities.Signals;
 
+import android.os.Bundle;
 import android.util.Log;
 
 @UnderConstruction(owner="Ikaro Silva")
 public class TEOAEProcedure extends TestProcedure{
 
 	private final String TAG = "TEOAECalibration";
-
+	private Bundle data;
 	private final double stimulusDuration=1;//stimulus duration
+	private short[] results;
+	private static final double  sampleFrequency = 44100;
+	private HashMap<String, Double> DPGRAM;
+	
 	public TEOAEProcedure(TestActivity parentActivity) {
 		super(parentActivity);
 		// TODO Auto-generated constructor stub
@@ -23,14 +32,9 @@ public class TEOAEProcedure extends TestProcedure{
 
 	@Override
 	public void run() {
-
-		short[] results;
-		double[] DPGRAM;
 		
 		clearLog();
 		sendMessage(TestActivity.Messages.PROGRESS);
-		
-		double sampleFrequency = 44100;
 		logToUI("Running TEOAE");
 		//create {f1, f2} tones in {left, right} channel of stereo stimulus
 		Log.v(TAG,"Generating stimulus");
@@ -54,19 +58,27 @@ public class TEOAEProcedure extends TestProcedure{
 		
         sendMessage(TestActivity.Messages.IO_COMPLETE); //Not exactly true becauase we delegate writing of file to another thread...
 		
-        DPGRAM=analyzeResults();
+        try {
+			DPGRAM = analyzeResults(results,sampleFrequency);
+		} catch (Exception e) {
+			Log.v(TAG,"Could not generate analysis for results!!" + e.getMessage());
+			e.printStackTrace();
+		}
 		sendMessage(TestActivity.Messages.ANALYSIS_COMPLETE);
 		
 		//TODO: Send data back to Activity, the final saving of result will be done when the Activity returns from plotting the processed
 		//data and the user accepts the results. So we need to delete the binary files if the user decides to reject/redo the test
-		//sendDPGRAMData(DPGRAM);
+		data=new Bundle();
+		data.putSerializable(AudioPulseDataAnalyzer.Results_MAP,DPGRAM);
+		sendMessage(data);
 		sendMessage(TestActivity.Messages.PROCEDURE_COMPLETE);
 	}
 	
-	private double[] analyzeResults() {
-		return null;
-		//TODO: define a TEOAEAnalysis call that operates on input arguments
-		//rather than a file location, call it from here
+	private HashMap<String, Double> analyzeResults(short[] data, double Fs) throws Exception {
+		 double epochTime=512; //Number of sample in which to break the FFT analysis
+		AudioPulseDataAnalyzer teoaeAnalysis=new TEOAEKempAnalyzer(data,Fs,epochTime);
+		return teoaeAnalysis.call();
+
 	}
 
 }
