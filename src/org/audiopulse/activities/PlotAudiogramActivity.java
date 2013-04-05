f027a083fa0025331e0d2aefc034c187d06fba98
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2013-04-05 02:59:28 +0000

    implementing an indeterminate dialog inside an alert dialog for when the user wants to save the data to disk.

--- src/org/audiopulse/activities/PlotAudiogramActivity.java
+++ src/org/audiopulse/activities/PlotAudiogramActivity.java
@@ -53,12 +53,17 @@ import org.audiopulse.io.AudioPulseXMLData;
 import org.audiopulse.utilities.AudioSignal;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.ProgressBar;
 
 public class PlotAudiogramActivity extends AudioPulseActivity {
 	/**
@@ -85,131 +90,160 @@ public class PlotAudiogramActivity extends AudioPulseActivity {
 			this.state=state; 
 		}
 	}
-	*/
+	 */
 
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			Bundle data = getIntent().getExtras();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Bundle data = getIntent().getExtras();
+		
+		Log.v(TAG,"extracting bundled data");
+		//Get Data generated according to the AudioPulseDataAnalyzer interface
+		//this should be a HashMap with keys defined in the interface
 
-			Log.v(TAG,"extracting bundled data");
-			//Get Data generated according to the AudioPulseDataAnalyzer interface
-			//this should be a HashMap with keys defined in the interface
+		results=(HashMap<String, Double>) data.getSerializable(AudioPulseDataAnalyzer.Results_MAP);
+		fileNames = (HashSet<String>) data.getSerializable(AudioPulseDataAnalyzer.MetaData_RawFileNames);
+		//Decode test name from the results and the mapping in the interface
+		Log.v(TAG,"results= "+ results.toString());
+		Log.v(TAG,"results keys= "+ results.keySet());
 
-			results=(HashMap<String, Double>) data.getSerializable(AudioPulseDataAnalyzer.Results_MAP);
-			fileNames = (HashSet<String>) data.getSerializable(AudioPulseDataAnalyzer.MetaData_RawFileNames);
-			//Decode test name from the results and the mapping in the interface
-			Log.v(TAG,"results= "+ results.toString());
-			Log.v(TAG,"results keys= "+ results.keySet());
+		testName=AudioPulseDataAnalyzer.testName.get(
+				results.get(AudioPulseDataAnalyzer.TestType).intValue());
 
-			testName=AudioPulseDataAnalyzer.testName.get(
-					results.get(AudioPulseDataAnalyzer.TestType).intValue());
-
-			//Loop through the bundle to get the results 
-			//The arrays should be interleaved, with odd samples representing X
-			//and even samples representing Y coordinates
-			ArrayList<Double> responseData= new ArrayList<Double>();
-			ArrayList<Double> noiseData= new ArrayList<Double>();
-			ArrayList<Double> stimData= new ArrayList<Double>();
-			for(String key: AudioPulseDataAnalyzer.responseKeys){
-				Double tmpdata = results.get(key);
-				if(tmpdata !=null){
-					//Get frequency
-					responseData.add(AudioPulseDataAnalyzer.frequencyMapping.get(key));
-					responseData.add(tmpdata);
-				}else{
-					Log.v(TAG,"null key: "+ key);
-				}
+		//Loop through the bundle to get the results 
+		//The arrays should be interleaved, with odd samples representing X
+		//and even samples representing Y coordinates
+		ArrayList<Double> responseData= new ArrayList<Double>();
+		ArrayList<Double> noiseData= new ArrayList<Double>();
+		ArrayList<Double> stimData= new ArrayList<Double>();
+		for(String key: AudioPulseDataAnalyzer.responseKeys){
+			Double tmpdata = results.get(key);
+			if(tmpdata !=null){
+				//Get frequency
+				responseData.add(AudioPulseDataAnalyzer.frequencyMapping.get(key));
+				responseData.add(tmpdata);
+			}else{
+				Log.v(TAG,"null key: "+ key);
 			}
-
-			for(String key: AudioPulseDataAnalyzer.noiseKeys){
-				Double tmpdata = results.get(key);
-				if(tmpdata !=null){
-					//Get frequency
-					noiseData.add(AudioPulseDataAnalyzer.frequencyMapping.get(key));
-					noiseData.add(tmpdata);
-				}else{
-					Log.v(TAG,"null key: "+ key);
-				}
-			}
-
-			for(String key: AudioPulseDataAnalyzer.stimKeys){
-				Double tmpdata = results.get(key);
-				if(tmpdata !=null){
-					//Get frequency
-					stimData.add(AudioPulseDataAnalyzer.frequencyMapping.get(key));
-					stimData.add(tmpdata);
-				}else{
-					Log.v(TAG,"null key: "+ key);
-				}
-			}
-
-			Log.v(TAG,"finished xtracting data from bundle");  	
-			PlotAudiogramView mView = new PlotAudiogramView(this,testName,responseData,noiseData,stimData);
-			requestWindowFeature(Window.FEATURE_NO_TITLE);
-			setContentView(mView);
 		}
 
+		for(String key: AudioPulseDataAnalyzer.noiseKeys){
+			Double tmpdata = results.get(key);
+			if(tmpdata !=null){
+				//Get frequency
+				noiseData.add(AudioPulseDataAnalyzer.frequencyMapping.get(key));
+				noiseData.add(tmpdata);
+			}else{
+				Log.v(TAG,"null key: "+ key);
+			}
+		}
 
-		@Override 
-		public boolean onKeyDown(int keyCode, KeyEvent event) {
+		for(String key: AudioPulseDataAnalyzer.stimKeys){
+			Double tmpdata = results.get(key);
+			if(tmpdata !=null){
+				//Get frequency
+				stimData.add(AudioPulseDataAnalyzer.frequencyMapping.get(key));
+				stimData.add(tmpdata);
+			}else{
+				Log.v(TAG,"null key: "+ key);
+			}
+		}
 
-			switch (keyCode) {
-			case KeyEvent.KEYCODE_BACK:
-			{
+		Log.v(TAG,"finished xtracting data from bundle");  	
+		PlotAudiogramView mView = new PlotAudiogramView(this,testName,responseData,noiseData,stimData);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(mView);
+	}
 
-				//Prompt user on how to continue based on the display of the analyzed results
-				AlertDialog dialog = new AlertDialog.Builder(this).create();
 
-				dialog.setMessage("Select an option");
+	@Override 
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-				dialog.setButton(DialogInterface.BUTTON_POSITIVE,"Exit",
-						new DialogInterface.OnClickListener(){
-					public void onClick(DialogInterface dialog, int which) {
-						//TODO: Pass a NULL URI
-						//if called from Sana
-						PlotAudiogramActivity.this.finish();
-					}
-				});
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_BACK:
+		{
 
-				dialog.setButton(DialogInterface.BUTTON_NEGATIVE,"Try Again",
-						new DialogInterface.OnClickListener(){
-					public void onClick(DialogInterface dialog, int which) {
-						// TODO: maybe do nothing here or clear state variables?  
-						//this should go back to the test activity by default
-						PlotAudiogramActivity.this.finish();
-					}
-				});
+			//Prompt user on how to continue based on the display of the analyzed results
+			AlertDialog dialog = new AlertDialog.Builder(this).create();
+			dialog.setMessage("Select an option");
 
-				dialog.setButton(DialogInterface.BUTTON_NEUTRAL,"Save & Exit",
-						new DialogInterface.OnClickListener(){
-					public void onClick(DialogInterface dialog, int which) {
-						//TODO: Implement saving file to disk (zipped) as  a thread
-						//and passing URI if called from Sana procedure
-						//TODO: We need to implement a timer
+			dialog.setButton(DialogInterface.BUTTON_POSITIVE,"Exit",
+					new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which) {
+					//TODO: Pass a NULL URI
+					//if called from Sana
+					PlotAudiogramActivity.this.finish();
+				}
+			});
 
-						/*
+			dialog.setButton(DialogInterface.BUTTON_NEGATIVE,"Try Again",
+					new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO: maybe do nothing here or clear state variables?  
+					//this should go back to the test activity by default
+					PlotAudiogramActivity.this.finish();
+				}
+			});
+
+			dialog.setButton(DialogInterface.BUTTON_NEUTRAL,"Save & Exit",
+					new DialogInterface.OnClickListener(){
+				public void onClick(DialogInterface dialog, int which) {
+					//TODO: Implement saving file to disk (zipped) as  a thread
+					//and passing URI if called from Sana procedure
+					//TODO: We need to implement a timer
+
+					final Handler mHandler = new Handler();
 					Log.v(TAG,"Saving files to disk");
+					showDialog(0);
+					
+					
+					//simulate work
+					/*
 					AudioPulseFileWriter writer= new AudioPulseFileWriter
 							(file,results);
 					Log.v(TAG,"saving raw data" );
 					writer.start();
-						 */
-						PlotAudiogramActivity.this.finish();
-					}
-				});
+					*/
+					
+				         // Start lengthy operation in a background thread
+				         new Thread(new Runnable() {
+				             public void run() {
+				            	 
+				                 for(int i=0;i<10;i++) {
+				                	 try {
+										Thread.sleep(500);//simulate work
+									} catch (InterruptedException e) {
+										e.printStackTrace();
+									} 
+				                 }
+				                 dismissDialog(0);
+				                 mHandler.post(this);
+				             }
+				         }).start();
+				         
+					PlotAudiogramActivity.this.finish();
 
-				dialog.show();
-				return true;
-			}
-
-			} //of switches
-
-			//exit activity
-			return super.onKeyDown(keyCode, event);
+				}
+			});
+			dialog.show();
+			return true;
 		}
 
+		} //of switches
 
-
-
+		//exit activity
+		return super.onKeyDown(keyCode, event);
 	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		ProgressDialog dialog = new ProgressDialog(this);
+		dialog.setTitle("Saving data");
+		dialog.setMessage("Please wait...");
+		dialog.setIndeterminate(true);
+		dialog.setCancelable(true);
+		return dialog;
+	}
+
+
+}
