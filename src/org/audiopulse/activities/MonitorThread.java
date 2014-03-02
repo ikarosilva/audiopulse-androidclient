201ea580c6780759d1ff89435d144829e92f27aa
ikarosilva00@gmail.com <ikarosilva00@gmail.com@a9b41606-45e7-9c5a-da78-67a032440075>
2014-03-02 15:38:26 +0000

    added detailed loggin message and starting test from main activity

--- src/org/audiopulse/activities/MonitorThread.java
+++ src/org/audiopulse/activities/MonitorThread.java
@@ -1,5 +1,8 @@
 package org.audiopulse.activities;
 
+import org.audiopulse.activities.MonitorHandler.Messages;
+import org.audiopulse.hardware.APulseIface;
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -8,29 +11,50 @@ import android.util.Log;
 public class MonitorThread extends Thread
 {
 	static final String TAG = "MonitorThread";
+	private final APulseIface apulse;
 	Handler mainThreadHandler = null;
 	int count = 0;
-	public MonitorThread(Handler h)
+	public MonitorThread(Handler h, APulseIface ap)
 	{
 		mainThreadHandler = h;
+		apulse=ap;
 	}
-	
+
 	@Override
 	public void run()
 	{
-		Log.v(TAG,"Starting thread task");
-		sendMessage("Monitor started");
-		Log.v(TAG,"started execution");
-		try {
-			Thread.sleep(2000);
-		}catch (InterruptedException e) {
-			e.printStackTrace();
+		sendMessage("Monitor started.\n");
+		int init = apulse.getStatus().test_state;
+		boolean isRunning=true;
+		switch (init) {
+		case APulseIface.APulseStatus.TEST_CONFIGURING:
+			sendMessage("Initial driver state is CONFIGURIN, waiting for main activity to start test.\n");
+			break;
+		case APulseIface.APulseStatus.TEST_READY:
+			sendMessage("Initial driver state is READY, waiting for main activity to start test...\n");
+			break;
+		case APulseIface.APulseStatus.TEST_RESET:
+			//Not sure what to do in a RESET state. For now expect the worse and assume it is 
+			//not possible to monitor the test
+			sendMessage("Initial driver state is in unexpected state: RESET, exiting monitor!\n");
+			isRunning=false;
+			break;
+		}
+
+		while(isRunning){
+			if(apulse.getStatus().test_state == APulseIface.APulseStatus.TEST_DONE){
+				isRunning=false;
+			}
+			try {
+				Thread.sleep(200);
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		sendMessage("Monitor finished");
-		
 
 	}
-	
+
 	private synchronized void sendMessage(String str){
 		Message msg = this.mainThreadHandler.obtainMessage(MonitorHandler.Messages.LOG);
 		Bundle b = new Bundle();
@@ -38,5 +62,5 @@ public class MonitorThread extends Thread
 		msg.setData(b);
 		this.mainThreadHandler.sendMessage(msg);
 	}
-	
+
 }
